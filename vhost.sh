660b7e7d02cc1eefa27428a31275f1d1e98225e0
rpritr <robi@foreach-labs.si>
2025-11-11 17:56:41 +0100

    add lab07

---
+++ vhost.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+        domain=$1
+        echo "Creating virtual host $domain"
+        echo "... creating virtual host file"
+
+        sudo touch /etc/apache2/sites-available/$domain.conf
+        sudo echo "<VirtualHost *:80>" >> /etc/apache2/sites-available/$domain.conf
+        sudo echo "ServerName $domain" >> /etc/apache2/sites-available/$domain.conf
+        sudo echo "ServerAlias $domain" >> /etc/apache2/sites-available/$domain.conf
+        sudo echo "DocumentRoot /var/www/$domain" >> /etc/apache2/sites-available/$domain.conf
+        sudo echo "ErrorLog \${APACHE_LOG_DIR}/$domain.error.log" >> /etc/apache2/sites-available/$domain.conf
+        sudo echo "CustomLog \${APACHE_LOG_DIR}/$domain.access.log combined" >> /etc/apache2/sites-available/$domain.conf
+
+        echo "Virtual host created successfully"
+
+        echo "Creating virtual host directory"
+        sudo mkdir /var/www/$domain
+
+        echo "... creating index.html"
+        sudo touch /var/www/$domain/index.html
+        sudo echo $domain >> /var/www/$domain/index.html
+
+        echo "Virtual host created successfuly"
+        exit 0
